51a4768bb648aeef10649311363ec65902bdd00d
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-05-15 12:43:44 +0200

    allow minimize

--- lib/zotero-citations.ts
+++ lib/zotero-citations.ts
@@ -3,10 +3,9 @@ declare const atom: any
 
 // import ZoteroCitationsView from './zotero-citations-view'
 
-// @ ts-ignore
+// @ts-ignore
 import { CompositeDisposable } from 'atom'
-
-import request = require('request-promise')
+import fetch from 'node-fetch'
 
 module.exports = {
   config: {
@@ -41,6 +40,11 @@ module.exports = {
       title: 'LaTeX citation command',
       description: 'When the style is set to LaTeX, use this command for the returned citations',
     },
+    minimize: {
+      type: 'boolean',
+      default: false,
+      title: 'Minimize Zotero after pick',
+    },
   },
 
   // zoteroCitationsView: null,
@@ -70,26 +74,27 @@ module.exports = {
     // this.zoteroCitationsView.destroy()
   },
 
-  pick() { // eslint-disable-line prefer-arrow/prefer-arrow-functions
-    const style = atom.config.get('zotero-citations.citationStyle')
-    const port = atom.config.get('zotero-citations.port')
-    const cmd = atom.config.get('zotero-citations.latexCommand') || 'cite'
-    const params = (style === 'LaTeX' ) ? `format=latex&command=${encodeURIComponent(cmd || 'cite')}` : `format=${encodeURIComponent(style)}`
-    const url = `http://127.0.0.1:${port}/better-bibtex/cayw?${params}`
+  async pick() { // eslint-disable-line prefer-arrow/prefer-arrow-functions
+    try {
+      const style = atom.config.get('zotero-citations.citationStyle')
+      const port = atom.config.get('zotero-citations.port')
+      const cmd = atom.config.get('zotero-citations.latexCommand') || 'cite'
+      const minimize = atom.config.get('zotero-citations.minimize') ? 'true' : 'false'
+      const params = (style === 'LaTeX' ) ? `format=latex&command=${encodeURIComponent(cmd || 'cite')}` : `format=${encodeURIComponent(style)}`
+      const url = `http://127.0.0.1:${port}/better-bibtex/cayw?${params}&minimize=${minimize}`
 
-    console.log(`Getting ${url}`)
-    request(url).then(result => {
-      atom.focus()
+      console.log(`Getting ${url}`)
+      const result = await (await fetch(url)).text()
       if (result) {
         const editor = atom.workspace.getActiveTextEditor()
         if (editor) editor.insertText(result)
       }
-
-    }).catch(err => {
       atom.focus()
+    }
+    catch (err) {
       console.log(`failed to fetch citation: ${err.message}`)
+      atom.focus()
       atom.notifications.addError('Zotero Citations: could not connect to Zotero. Are you sure it is running?')
-
-    })
+    }
   },
 }
